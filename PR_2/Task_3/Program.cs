3aae35183f0ca490cc78c626b46a2cb8a9c7547e
Nikita Kazantsev <nik.09.05.05@gmail.com>
2024-03-10 15:15:58 +0200

    add folder PR_2

---
+++ PR_2/Task_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayX = new int[11] { 5, 2, 7, 1, 9, 3, 8, 4, 6, 11, 10, 13, 19, 14, 18, 17, 12 };
+
+        Console.WriteLine("Array X:");
+        PrintArray(arrayX);
+
+        int minValue = FindMinValue(arrayX);
+        Console.WriteLine($"Min: {minValue}");
+
+        int maxValue = FindMaxValue(arrayX);
+        Console.WriteLine($"Max: {maxValue}");
+
+        Console.ReadLine();
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int element in arr)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static int FindMinValue(int[] arr)
+    {
+        int minValue = arr[0];
+        foreach (int element in arr)
+        {
+            if (element < minValue)
+            {
+                minValue = element;
+            }
+        }
+        return minValue;
+    }
+
+    static int FindMaxValue(int[] arr)
+    {
+        int maxValue = arr[0];
+        foreach (int element in arr)
+        {
+            if (element > maxValue)
+            {
+                maxValue = element;
+            }
+        }
+        return maxValue;
+    }
+}
